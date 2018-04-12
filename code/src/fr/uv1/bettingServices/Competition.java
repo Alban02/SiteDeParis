e56dc8d92511353ff508190d93967d55b4a53c75
Vincent Lopes <vincent.lopes@imt-atlantique.net>
2018-04-12 15:52:21 +0200

    Competition class almost done

--- code/src/fr/uv1/bettingServices/Competition.java
+++ code/src/fr/uv1/bettingServices/Competition.java
@@ -3,26 +3,92 @@ package fr.uv1.bettingServices;
 import fr.uv1.bettingServices.exceptions.CompetitionException;
 import fr.uv1.bettingServices.exceptions.ExistingCompetitorException;
 
-import java.util.Collection;
+import java.util.HashSet;
 
 public class Competition {
 
-    Collection<Competitor> listCompetitors;
+    HashSet<Competitor> competitorsList;
+    HashSet<Bet> betsList;
+    int competitionNumber;
+    String name;
+    MyCalendar closingDate;
 
-
+    public Competition(int competitionNumber, String name, MyCalendar closingDate) {
+        this.competitionNumber = competitionNumber;
+        this.name = name;
+        this.closingDate = closingDate;
+        competitorsList = new HashSet<Competitior>();
+        betsList = new HashSet<Bet>();
+    }
 
     private boolean competitionEnded (){
-        return false;
+        return closingDate.isInThePast();
     }
 
     private boolean competitorExist (Competitor competitor) {
-        return false;
+        return competitorsList.contain(competitor);
+    }
+
+    @Override
+    public String toString() {
+    
+    }
+
+    public void settlePodium(Competitor first, Competitor second, Competitor third) {
+        Iterator it = betsList.iterator();
+        while (it.hasNext()) {
+            Bet bet = it.next();
+            Subscriber subscriber = bet.getSubscriber();
+            if (bet.getCompetitor().get(0).equals(first) && bet.getCompetitor().get(1).equals(second) && bet.getCompetitor().get(2).equals(third)) {
+                subscriber.creditSubscriber(bet.getStake());
+            }
+            subscriber.removeBet(bet);
+        }    
+    }
+
+    public void settleWinner(Competitor winner) {
+        Iterator it = betsList.iterator();
+        while (it.hasNext()) {
+            Bet bet = it.next();
+            Subscriber subscriber = bet.getSubscriber();
+            if (bet.getCompetitor().get(0).equals(winner)) {
+                subscriber.creditSubscriber(bet.getStake());
+            }
+            subscriber.removeBet(bet);
+        }
+    }
+
+    public void addCompetitor(Competitor c) {
+        competitorsList.add(c);
     }
 
     public void deleteCompetitor (Competitor competitor) throws  CompetitionException, ExistingCompetitorException {
-        if (this.competitionEnded()) throw new CompetitionException();
-        if (!(this.competitorExist(competitor))) throw new ExistingCompetitorException() ;
-        this.listCompetitors.remove(competitor);
-
+        if (this.competitionEnded())
+            throw new CompetitionException();
+        if (!(this.competitorExist(competitor)))
+            throw new ExistingCompetitorException();
+        competitorsList.remove(competitor);
     }
+
+    public cancelAllBets() {
+        Iterator it = betsList.iterator();
+        while (it.hasNext()) {
+            Bet bet = it.next();
+            bet.getSubscriber().cancelBet(bet);
+        }
+    }
+
+    public boolean equals(Object o) {
+        if (this == 0)
+            return true;
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        Competition c = (Competition) o;
+        return c.name == name;
+    }
+
+
 }
+
